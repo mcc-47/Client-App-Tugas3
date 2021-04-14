aa7d6b84d6e1cbaa2b985b402ed4705c14c8660d
Jaka Brajadenta <jakabrajadenta@gmail.com>
2021-04-15 01:19:20 +0700

    create button berfungsi

--- src/main/resources/static/js/alert.js
+++ src/main/resources/static/js/alert.js
@@ -1,8 +1,16 @@
 
+//ONLY CHECKING ID
+function cekId(){
+    let del = $('#upd').attr('href');
+    console.log(upd);
+}
+
+//DELETE BUTTON
 function onClickDelete(){
     event.preventDefault();
     let del = $('#del').attr('href');
     console.log(del);
+    
     Swal.fire({
         title: 'Are you sure?',
         text: "You won't be able to revert this!",
@@ -11,78 +19,33 @@ function onClickDelete(){
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
         confirmButtonText:'Yes, delete it!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-            const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.addEventListener('mouseenter', Swal.stopTimer)
-              toast.addEventListener('mouseleave', Swal.resumeTimer)
-            }
-          })
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+                })
 
-          Toast.fire({
-            icon: 'success',
-            title: 'Signed Out successfully'
-          })
-          document.location.href = del;
-          //del.remove;
-        }
-    });
+                Toast.fire({
+                    icon: 'success',
+                    title: 'Signed Out successfully'
+                })
+                document.location.href = del;
+                }
+        });
 }
-function logout(){
+
+function onClickCreate(){
+    
     event.preventDefault();
-    let out = $('#out').attr('href');
-    console.log(out);
-    Swal.fire({
-        position : 'top-end',
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText:'Log out'
-      }).then((result) => {
-        if (result.isConfirmed) {
-            const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.addEventListener('mouseenter', Swal.stopTimer)
-              toast.addEventListener('mouseleave', Swal.resumeTimer)
-            }
-          })
-
-          Toast.fire({
-            icon: 'success',
-            title: 'Signed Out successfully'
-          })
-          document.location.href = out;
-          //del.remove;
-        }
-    });
-}
-
-function cekId(){
-    let del = $('#upd').attr('href');
-    console.log(upd);
-}
-
-function onClickUpdate(){
-    //event.returnValue = false;
-    //console.log(event);
-    //const href = document.querySelector('#update').getAttribute('action');
-    //console.log(href);
-    event.preventDefault();
-    let form = $('form');
+    let form = $('#createForm');
     console.log(form);
     Swal.fire({
         title: 'Are you sure?',
@@ -91,18 +54,16 @@ function onClickUpdate(){
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
         confirmButtonText:'Yes, Submit!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          //event.returnValue = true;
-          Swal.fire(
-            'Updated!',
-            'Your file has been submitted.',
-            {timer: 2000}
-          );
-          form.submit();
-          
-        }
-    });
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Updated!',
+                    'Your file has been submitted.',
+                    {timer: 2000}
+                );
+                form.submit();
+            }
+        });
 }
 
 function onClickChange(){
@@ -112,20 +73,69 @@ function onClickChange(){
         title: 'Your work has been saved',
         showConfirmButton: false,
         timer: 1500
-      });
+    });
 }
 
+function onClickSimpleCreate(){
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Your work has been saved',
+        showConfirmButton: false,
+        timer: 2000
+    });
+}
+
+//LOGOUT BUTTON
+function logout(){
+    event.preventDefault();
+    let out = $('#out').attr('href');
+    console.log(out);
+    
+    Swal.fire({
+        position : 'top-end',
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText:'Log out'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+                })
+
+                Toast.fire({
+                    icon: 'success',
+                    title: 'Signed Out successfully'
+                })
+                document.location.href = out;
+            }
+        });
+}
+
+//LOGIN BUTTON
 function login(){
     const Toast = Swal.mixin({
-      toast: true,
-      position: 'top-end',
-      showConfirmButton: false,
-      timer: 3000,
-      timerProgressBar: true,
-      didOpen: (toast) => {
-        toast.addEventListener('mouseenter', Swal.stopTimer)
-        toast.addEventListener('mouseleave', Swal.resumeTimer)
-      }
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
     })
 
     Toast.fire({
@@ -133,4 +143,3 @@ function login(){
       title: 'Signed In successfully'
     })
 }
-
